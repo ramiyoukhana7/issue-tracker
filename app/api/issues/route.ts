ea8d39879058eb1a201398ed01236b2c6f89a35d
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-15 21:38:40 +0200

    Building the POST api

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// Importing Zod to validate the API
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  // giving the title a type and setting the minimum and maximum title length
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    // If the issue is unsuccessful show the validation error and set the status to 400
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  // If the issue is successful create an issue, store the data and set the status to 201
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
---
+++ prisma/client.ts
@@ -0,0 +1,19 @@
+// This file ensures that we do not have multipale instaces of prisma client running
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

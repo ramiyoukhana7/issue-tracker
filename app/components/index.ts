9479dc6d31fdcab2b2ef2525d9b4bd054dcd82dd
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-17 19:34:05 +0200

    Refactoring and organizing imports

---
+++ app/components/index.ts
@@ -0,0 +1,7 @@
+// Importing components and exporting them, to re-use these files in a more organized fashion
+
+export { default as Link } from "./Link";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as IssueStatusBadge } from "./IssueStatusBadge";
+export { default as Spinner } from "./Spinner";
+export { default as Skeleton } from "./Skeleton";
--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,7 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { Heading, Flex, Card, Box } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
 
 // Placeholder loading file to remove the previous loading skeleton for this route
 const LoadingIssueDetailPage = () => {

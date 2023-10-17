d45080681b6994f126e66a7503efe0d6825cce43
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-17 19:03:49 +0200

    Setting up more loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,8 +1,16 @@
+import { Box } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 // Placeholder loading file to remove the previous loading skeleton for this route
 const LoadingNewIssuePage = () => {
-  return <div>Loading ...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+    </Box>
+  );
 };
 
 export default LoadingNewIssuePage;

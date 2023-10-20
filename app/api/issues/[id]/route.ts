e9b1ece0faea6cc1961b634bb1a7a9f7d9a171e4
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-20 18:45:16 +0200

    Removed security changes, causing errors

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,18 +1,12 @@
-import authOptions from "@/app/auth/authOptions";
 import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) return NextResponse.json({}, { status: 401 });
   const body = await request.json();
-  // We take the body and validate it with issueSchema.
   const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
@@ -30,23 +24,16 @@ export async function PATCH(
 
   // findUnique function looks for a specific ID.
   const issue = await prisma.issue.findUnique({
-    // the 'where' specifes that the id should match the parsed params.id value
     where: { id: parseInt(params.id) },
   });
   if (!issue)
     return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
-  // If the issue is truthy, the issue updates title and description using the update() prisma function
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
       title: body.title,
       description: body.description,
-      assignedToUser: {
-        connect: {
-          id: assignedToUserId,
-        },
-      },
     },
   });
 
@@ -57,9 +44,6 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) return NextResponse.json({}, { status: 401 });
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });

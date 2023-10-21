41eede59213af64958d61bf4e85d12593ea189f4
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-21 19:13:24 +0200

    Fixing linting errors

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 // We add NextRequest to prevent caching
-export async function GET(request: NextRequest) {
+export async function GET(_request: NextRequest) {
   const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
   return NextResponse.json(users);
 }
